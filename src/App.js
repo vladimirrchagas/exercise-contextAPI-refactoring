003bcaaf94fa16b2bac0caec2ff560f259e455f7
vladimirrchagas <vladimirrchagas@gmail.com>
2021-01-19 10:25:11 -0300

    exercise one completed using Context API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component, createContext } from 'react';
 import './App.css';
 import Cars from './Cars';
 
-function App() {
-  return (
-    <Cars />
-  );
+export const MyContext = createContext();
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cars: {
+        red: false,
+        blue: false,
+        yellow: false,
+      }
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+
+  moveCar(car, status) {
+    this.setState({ ...this.state, cars: { ...this.state.cars, [car]: status } });
+  };
+
+  render () {
+    const contextValue = {
+      car: this.state.cars,
+      moveCar: this.moveCar,
+    };
+
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,71 +1,64 @@
 // src/Cars.jsx
 
-import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
-import { moveCar } from './redux/actionCreators';
+// import { moveCar } from './redux/actionCreators';
+import { MyContext } from './App';
 
-function Cars({ redCar, blueCar, yellowCar, moveCar }) {
+function Cars() {
   return (
-    <div>
+    <MyContext.Consumer>
+    { (value) => {
+    const { car, moveCar } = value;
+    const { red: redCar, blue: blueCar, yellow: yellowCar } = car;
+    return (
       <div>
-        <img
-          className={redCar ? 'car-right' : 'car-left'}
-          src={carRed}
-          alt="red car"
-        />
-        <button
-          onClick={() => moveCar('red', !redCar)}
-          type="button"
-        >
-          Move
-        </button>
+        <div>
+          <img
+            className={redCar ? 'car-right' : 'car-left'}
+            src={carRed}
+            alt="red car"
+          />
+          <button
+            onClick={() => moveCar('red', !redCar)}
+            type="button"
+          >
+            Move
+          </button>
+        </div>
+        <div>
+          <img
+            className={blueCar ? 'car-right' : 'car-left'}
+            src={carBlue}
+            alt="blue car"
+          />
+          <button
+            onClick={() => moveCar('blue', !blueCar)}
+            type="button"
+          >
+            Move
+          </button>
+        </div>
+        <div>
+          <img
+            className={yellowCar ? 'car-right' : 'car-left'}
+            src={carYellow}
+            alt="yellow car"
+          />
+          <button
+            onClick={() => moveCar('yellow', !yellowCar)}
+            type="button"
+          >
+            Move
+          </button>
+        </div>
       </div>
-      <div>
-        <img
-          className={blueCar ? 'car-right' : 'car-left'}
-          src={carBlue}
-          alt="blue car"
-        />
-        <button
-          onClick={() => moveCar('blue', !blueCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={yellowCar ? 'car-right' : 'car-left'}
-          src={carYellow}
-          alt="yellow car"
-        />
-        <button
-          onClick={() => moveCar('yellow', !yellowCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-    </div>
+    )}}
+    </MyContext.Consumer>
   );
 }
 
-Cars.propTypes = {
-  moveCar: PropTypes.func.isRequired,
-  blueCar: PropTypes.bool.isRequired,
-  redCar: PropTypes.bool.isRequired,
-  yellowCar: PropTypes.bool.isRequired,
-};
-
-const mapStateToProps = (state) => ({
-  redCar: state.cars.red,
-  blueCar: state.cars.blue,
-  yellowCar: state.cars.yellow});
-
-const mapDispatchToProps = { moveCar };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+export default Cars;
